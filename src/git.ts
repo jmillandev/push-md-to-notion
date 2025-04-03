3a30aaed449fe439d1c2d4b502fa6781d3039973
Jesús Millán <jgmc3012@gmail.com>
2025-04-02 22:17:52 -0300

    feat: log changed markdown files in last commit

--- src/git.ts
+++ src/git.ts
@@ -1,16 +1,23 @@
 import { execSync } from 'node:child_process';
+import * as core from '@actions/core';
 
 /**
  * Query git repo for any markdown files that have changed in the last commit.
  * @returns List of markdown files.
  */
 export function getChangedMdFiles(): string[] {
-  const gitOutput = execSync('git show --name-only --pretty=format:', {
+  core.startGroup('get changed markdown files');
+  const gitCommand = 'git show --name-only --pretty=format:';
+  const gitOutput = execSync(gitCommand, {
     encoding: 'utf-8',
   });
-
-  return gitOutput
-    .trim()
-    .split('\n')
-    .filter((fn) => fn.endsWith('.md'));
+  console.log(gitCommand);
+  console.log(gitOutput);
+  const markdownChanges = gitOutput
+      .trim()
+      .split('\n')
+      .filter((fn: string) => fn.endsWith('.md'));
+  console.info(`Markdown files changed in last commit: ${markdownChanges}`);
+  core.endGroup()
+  return markdownChanges
 }
